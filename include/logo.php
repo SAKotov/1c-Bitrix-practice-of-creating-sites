b3c90e6a934add29d01de1064670661e921107c6
SAKotov <mozz.kotov@gmail.com>
2021-09-22 22:55:59 +0300

    Create a flow-through include area for the logo

---
+++ include/logo.php
@@ -0,0 +1,3 @@
+<div class="logo">
+ <a href="/"><img width="164" alt="logo.png" src="/upload/medialibrary/ff2/vjjec3qz9c21snur5c63n1unn1nvbl8v.png" height="51" title="logo.png" class="img-responsive"></a>
+</div>
