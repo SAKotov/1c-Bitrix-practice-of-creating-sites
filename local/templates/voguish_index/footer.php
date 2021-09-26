7d71f033e15caa0f445d45c4cc99a7814d6a36ce
SAKotov <mozz.kotov@gmail.com>
2021-09-26 12:32:41 +0300

    creating an infoblock blog, creating a template for displaying a blog

--- local/templates/voguish_index/footer.php
+++ local/templates/voguish_index/footer.php
@@ -66,6 +66,7 @@
     </div>
 </div>
 <div class="clearfix"> </div>
+<?php if ($APPLICATION->GetCurPage(false) == '/'): ?>
 <div class="fle-xsel">
     <ul id="flexiselDemo3">
         <li>
@@ -143,6 +144,7 @@
     <script type="text/javascript" src="<?=SITE_TEMPLATE_PATH;?>/js/jquery.flexisel.js"></script>
     <div class="clearfix"> </div>
 </div>
+<?php endif; ?>
 <div class="footer">
     <div class="col-md-3 foot-1">
         <h4>Quick Links</h4>
